cf82d7ed025228aaef03e2d927dee73357a7a36d
EgJo-08 <U2535495@rts-365.dk>
2026-03-10 09:44:39 +0100

    har tilføjet sådan at man kan lave diver og tilføje number

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const listElement = document.querySelector("#drink_list")
 const knap = document.querySelector("#knap")
 
+let number = 0
+
 
 knap.addEventListener("click", tilføj)
 
@@ -8,25 +10,49 @@ knap.addEventListener("click", tilføj)
 function tilføj() {
     const listitem = document.createElement("div");
 
+
+
     listitem.innerHTML = `
                     <div class="items">
                     <input type="file" class="imageInput" accept="image/png, image/jpeg">
                     <img class="preview" src="">
                     <input type="text" class="navn" placeholder="navn">
-                    <p>number:</p>
-                    <button>tilføj</button>
-                    <button>fjern</button>
+                    <p>number:${number}</p>
+                    <button class="add">tilføj</button>
+                    <button class="remove">fjern</button>
                     </div>
                 `;
     listElement.appendChild(listitem)
-      const fileInput = listitem.querySelector(".imageInput")
+    const fileInput = listitem.querySelector(".imageInput")
     const img = listitem.querySelector(".preview")
 
-    fileInput.addEventListener("change", function(){
+    fileInput.addEventListener("change", function () {
         const file = this.files[0]
-        if(file){
+        if (file) {
             img.src = URL.createObjectURL(file)
         }
     })
-}
+    const countText = listitem.querySelector("p")
 
+    let add = listitem.querySelector(".add")
+
+    add.addEventListener("click", number_drinks)
+
+    function number_drinks() {
+    const number_tilføj = document.createElement("input")
+    number_tilføj.type = "number"
+
+    listitem.appendChild(number_tilføj)
+
+    number_tilføj.addEventListener("keydown", function (e) {
+        if (e.key === "Enter") {
+
+            number += Number(number_tilføj.value)
+
+            countText.textContent = `number: ${number}`
+
+            number_tilføj.remove() 
+        }
+    })
+} 
+}
